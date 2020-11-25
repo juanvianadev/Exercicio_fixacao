525bbe87b319b49313a1a1455fbaa995a6265a9b
Juan <juanviana2577@gmail.com>
2020-11-24 22:10:14 -0300

    Exercicio1 Finalizado

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o ano atual: ");
+            int anoAtual = int.Parse(Console.ReadLine());;
+
+            Console.WriteLine("Digite a data de nascimento: ");
+            int nascimento = int.Parse(Console.ReadLine());
+            
+            int validacao = anoAtual - nascimento;
+
+            if(validacao == 18){
+                Console.WriteLine("Você tem que votar esse ano.");
+            if(validacao > 16){
+                Console.WriteLine("Seu Voto é opcional");
+                }
+            } else {
+                Console.WriteLine("Você não pode votar esse ano.");
+            }
+
+        }
+    }
+}
